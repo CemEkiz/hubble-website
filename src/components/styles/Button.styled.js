c9f2dba23faa67ea1fed934254ff869cea2c69ef
Cem Ekiz <ekizcem.pro@gmail.com>
2022-08-22 15:19:39 +0200

    update the button box shadow

--- src/components/styles/Button.styled.js
+++ src/components/styles/Button.styled.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 export const Button = styled.button`
 	border-radius: 50px;
 	border: none;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+	box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 	cursor: pointer;
 	font-size: 16px;
 	font-weight: 700;
 	padding: 15px 60px;
 	background-color: ${({ bg }) => bg || '#fff'};
 	color: ${({ color }) => color || '#333'};
+	transition: all 0.2s;
 
 	&:hover {
 		opacity: 0.9;
